b6500163afdcef44b3e04465d4c90554aa195556
Paul <lapaul86@yahoo.com>
2024-05-12 18:45:43 +0200

    ex 3 terminat

---
+++ ex3/index.js
@@ -0,0 +1,19 @@
+// modul 1
+
+const numbers = ['2', '10', '12', '20', '25']
+
+for (let i = 0; i < numbers.length; i += 1) {
+    const index = (numbers.length - 1) - i;
+    console.log(numbers[index]);
+
+//}
+
+
+
+//modul 2
+
+//const numbers = ['2', '10', '12', '20', '25']
+
+//for (let i = numbers.length - 1; i >= 0; i -= 1) {
+//    console.log(numbers[i]);
+//}

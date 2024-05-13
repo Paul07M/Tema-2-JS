7b0bd8b90eb2e6ba2642cd8c8a13a3d2cd1c8c2b
Paul <lapaul86@yahoo.com>
2024-05-13 16:32:47 +0200

    exercitiu terminat

--- ex2/index.js
+++ ex2/index.js
@@ -1,4 +1,13 @@
 
+const numbers = [2, -5, 10, 16, 40];
 
+let sum = numbers.reduce(all_numbers);
+
+function all_numbers(total, value, index, array) {
+
+    return total + value;
+}
+
+console.log("Aceasta este suma " + sum);
 
 
